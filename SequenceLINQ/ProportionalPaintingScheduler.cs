6f405cd1ca2aee8d75b296ad40fbf422cc52bd1a
daretodreamz <bing.wang2016@hotmail.com>
2016-11-17 10:56:28 +1300

    Area assigned to each painter will be in proportion to that painter's velocity compare to the total volocity of the entire group

--- SequenceLINQ/ProportionalPaintingScheduler.cs
+++ SequenceLINQ/ProportionalPaintingScheduler.cs
@@ -15,14 +15,16 @@ namespace SequenceLINQ
                 .Select(painter =>
                         Tuple.Create(painter, sqMeters / painter.EstimateTimeToPaint(sqMeters).TotalHours))
                         .ToList();
+
             double totalVelocity = velocities.Sum(tuple => tuple.Item2);
 
+            // Area assigned to each painter will be in proportion to that painter's velocity compare to the total volocity of the entire group
             IEnumerable<PaintingTask<ProportionalPainter>> schedule =
                 velocities
                 .Select(tuple =>
                     new PaintingTask<ProportionalPainter>(
                         tuple.Item1,
-                        sqMeters * tuple.Item2 / totalVelocity))
+                        sqMeters * ( tuple.Item2 / totalVelocity) ))
                         .ToList();
 
             return schedule;
